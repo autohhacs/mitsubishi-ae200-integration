fa6e669e476331a7b703358f4067879553256233
autohhacs <hacs@autoh.cloud>
2025-06-23 05:55:57 -0400

    Create const.py

---
+++ custom_components/mitsubishi_ae200/const.py
@@ -0,0 +1,13 @@
+"""Constants for the AutoH Mitsubishi AE200 integration."""
+
+DOMAIN = "mitsubishi_ae200"
+
+CONF_CONTROLLER_ID = "controller_id"
+CONF_IP_ADDRESS = "ip_address"
+CONF_TEMPERATURE_UNIT = "temperature_unit"
+
+DEFAULT_NAME = "AutoH Mitsubishi AE200"
+
+# Temperature unit options
+TEMP_CELSIUS = "celsius"
+TEMP_FAHRENHEIT = "fahrenheit"
