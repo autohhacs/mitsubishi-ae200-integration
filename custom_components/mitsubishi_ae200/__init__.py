dc6aabd9547cdf1cce213b8f451a2007e5a21116
autohhacs <hacs@autoh.cloud>
2025-06-29 10:38:35 -0400

    Update __init__.py

--- custom_components/mitsubishi_ae200/__init__.py
+++ custom_components/mitsubishi_ae200/__init__.py
@@ -1,28 +1,20 @@
-"""The AutoH Mitsubishi AE200 integration."""
-import logging
-
+"""Minimal working integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
 
 DOMAIN = "mitsubishi_ae200"
-
-_LOGGER = logging.getLogger(__name__)
-
 PLATFORMS = [Platform.CLIMATE]
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up AutoH Mitsubishi AE200 from a config entry."""
+    """Set up from config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
+    """Unload config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
