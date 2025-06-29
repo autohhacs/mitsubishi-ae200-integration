0ffe47fa974f8bb26169c6679b0e3d978890e0e2
autohhacs <hacs@autoh.cloud>
2025-06-29 13:02:09 -0400

    Add files via upload

---
+++ custom_components/mitsubishi_ae200/const.py
@@ -0,0 +1,12 @@
+"""Constants for the AutoH Mitsubishi AE200 integration."""
+
+DOMAIN = "mitsubishi_ae200"
+
+CONF_CONTROLLER_ID = "controller_id"
+CONF_IP_ADDRESS = "ip_address"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_TEMPERATURE_UNIT = "temperature_unit"
+
+TEMP_CELSIUS = "celsius"
+TEMP_FAHRENHEIT = "fahrenheit"
